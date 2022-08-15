bc85bc8e4953e0fa1820fe697b2faedec5c2a041
ValerianMemsk <valera.goncharenko@hotmail.com>
2022-08-15 19:58:23 +0300

    фикс миграции

--- database/migrations/2022_08_14_160132_create_sites_table.php
+++ database/migrations/2022_08_14_160132_create_sites_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('site', function (Blueprint $table) {
+        Schema::create('sites', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('url');
