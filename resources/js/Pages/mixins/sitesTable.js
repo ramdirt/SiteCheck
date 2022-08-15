6f4d563957533803dde19374c4e28862b07db7c6
dmrompav <dm.rom.pav@gmail.com>
2022-08-16 02:11:04 +0500

    SC-D-1 получение сайтов с бэка и форма для добавления новых

---
+++ resources/js/Pages/mixins/sitesTable.js
@@ -0,0 +1,92 @@
+export default {
+    data() {
+        return {
+            columns: [
+
+                // NAME COLUMN
+                {
+                    title: 'Сайт',
+                    key: 'name',
+                    sortable: true,
+                    render: (h, { row }) => {
+                        return h('div',
+                            {
+                                class: 'flex items-center space-x-2',
+                            },
+                            [
+                                h('div', {
+                                    style: {
+                                        background: this.rowAcceptColorClass(row.accepted, row.tests)
+                                    },
+                                    class: 'w-8 h-8 rounded-full border'
+                                }),
+                                h('a', {
+                                    href: row.url,
+                                    target: '_blank'
+                                }, row.name)
+                            ]
+                        );
+                    }
+                },
+
+                // TESTS COLUMN
+                {
+                    title: 'Состояние',
+                    key: 'tests',
+                    sortable: true,
+                    render: (h, { row }) => {
+                        if (row.accepted && row.tests) {
+                            return h('div', [
+                                h('span', `Успешных проверок ${row.accepted} из ${row.tests}`),
+                            ]);
+                        }
+
+                        return h('div', [
+                            h('span', `Нет данных о проверках`),
+                        ]);
+                    }
+                },
+
+                // ACTION COLUMN
+                {
+                    title: ' ',
+                    key: 'action',
+                    fixed: 'right',
+                    width: 160,
+                    render: (h, params) => {
+                        return h('div', [
+                            h('a', {
+                                args: {
+                                    // TODO route
+                                    href: route('')
+                                }
+                            }, 'Подробнее...'),
+                        ]);
+                    }
+                }
+
+            ]
+        }
+    },
+
+    methods: {
+        rowAcceptColorClass(accepted, tests) {
+            if (!tests) {
+                return 'transparent'
+            }
+
+            const paletDiff = [[510, 0, 0], [0, 510, 0]];
+
+            if (accepted > tests) accepted = tests;
+            if (!accepted) accepted = 0;
+
+            const dr = paletDiff[0][0] - ((paletDiff[0][0] - paletDiff[1][0]) * accepted / tests);
+            const dg = paletDiff[0][1] - ((paletDiff[0][1] - paletDiff[1][1]) * accepted / tests);
+
+            const r = dr > 255 ? 255 : dr;
+            const g = dg > 255 ? 255 : dg;
+
+            return `rgb(${r}, ${g}, 0)`;
+        }
+    }
+};
